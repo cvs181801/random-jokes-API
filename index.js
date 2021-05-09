ab4b9a18ecad19483f027f23035e74166e677910
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-09 16:11:11 -0700

    grab elements and build API

---
+++ index.js
@@ -0,0 +1,7 @@
+//grab the DOM elements and save to variables so we can create functionality!
+
+const setupDiv = document.getElementById('setup');
+const punchlinDiv = document.getElementById('punchline');
+const punchlineBtn = document.getElementById('punchline-button');
+const newJokeBtn = document.getElementById('new-joke-button');
+
