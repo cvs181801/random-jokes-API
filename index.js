8c395392acfef79054604a8e3c0d4fafdcf80dd4
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-10 17:58:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //grab the DOM elements and save to variables so we can create functionality!
 
 const setupDiv = document.getElementById('setup');
-const punchlinDiv = document.getElementById('punchline');
+const punchlineDiv = document.getElementById('punchline');
 const punchlineBtn = document.getElementById('punchline-button');
 const newJokeBtn = document.getElementById('new-joke-button');
 
