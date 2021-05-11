be70913f7eb5456eac3f71c6aa2c82a5d8921e11
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-10 17:55:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const punchlinDiv = document.getElementById('punchline');
 const punchlineBtn = document.getElementById('punchline-button');
 const newJokeBtn = document.getElementById('new-joke-button');
 
+//create a variable to store the punchline so it can be reassigned as we go
+let punchline;
+
+//make the 'get punchline' button work
+
+punchlineBtn.addEventListener('click', getPunchline);
+
+function getPunchline() {
+    punchlineDiv.innerHTML = punchline;
+    punchlineDiv.classList.add('bubble');
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+}
+//get the joke data from the API
+
 async function getJoke() {
     const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
     const joke = await jokePromise.json();
